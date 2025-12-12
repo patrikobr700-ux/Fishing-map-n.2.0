7c5c298993dcc91b93db34b08a323d15d2d2ae3f
patrikobr700-ux <patrikobr700@gmail.com>
2025-12-12 11:44:26 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,36 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
+const CACHE_NAME = 'fishingmap-v1';
+const ASSETS = [
+  '/index.html',
+  '/manifest.webmanifest',
+  '/datapoints.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)).then(()=> self.skipWaiting()));
 });
 
-self.addEventListener("activate", () => {
-  console.log("Service Worker aktivní.");
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  const url = new URL(req.url);
+
+  if (req.mode === 'navigate' || url.pathname.endsWith('index.html')) {
+    event.respondWith(
+      fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+        return res;
+      }).catch(() => caches.match('/index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(caches.match(req).then(cached => cached || fetch(req).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE_NAME).then(cache => cache.put(req, copy));
+    return res;
+  })).catch(()=>{}));
 });
